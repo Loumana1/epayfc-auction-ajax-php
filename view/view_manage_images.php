48d299a54e85ef4e5c0f447b059ee96848c17a2c
sam_prophete <0609sansengimana@student.epfc.eu>
2026-02-01 16:19:35 +0100

    (manage_images)last update css

--- view/view_manage_images.php
+++ view/view_manage_images.php
@@ -20,13 +20,15 @@
     </div> 
 
     <main>
+        <h1 class="page-subtitle">Manage Images for "<?= htmlspecialchars($item->get_Title()) ?>"</h1>
+        
              <?php include __DIR__ . "/partials/manage_images/_manage_images.php"; ?>
 
     </main>
 
-        <<?php include __DIR__ . "/partials/_navbar.php"; ?>
+        <?php include __DIR__ . "/partials/_navbar.php"; ?>
 
          <?php include __DIR__ . "/partials/_timebar.php"; ?>
         
-        </body>
+</body>
 </html>
