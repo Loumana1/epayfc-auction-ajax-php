e4d81361f989fe8a5fd6632e9085a15a17e011bd
zietraore <2610zitraore@student.epfc.eu>
2026-02-06 19:11:18 +0100

    refactor(sales): variables snake_case

--- controller/ControllerSales.php
+++ controller/ControllerSales.php
@@ -10,17 +10,17 @@ class ControllerSales extends Controller {
 
     public function index(): void {
 
-        $currentUser = $this->get_user_or_redirect();
-        $userId = $currentUser->get_Id();
+        $current_user = $this->get_user_or_redirect();
+        $user_id = $current_user->get_Id();
         
         
         $now = AppTime::get_current_datetime();
         
         
-        $statistics = Item::get_sales_statistics($userId, $now);
+        $statistics = Item::get_sales_statistics($user_id, $now);
         
       
-        $sold_items_raw = Item::get_sold_items_by_owner($userId, $now);
+        $sold_items_raw = Item::get_sold_items_by_owner($user_id, $now);
         
     
         $sold_items = [];
@@ -28,24 +28,24 @@ class ControllerSales extends Controller {
             if (!$item instanceof Item) continue;
             
            
-            $mainPicture = ItemPicture::get_main_picture($item->get_Id());
+            $main_picture = ItemPicture::get_main_picture($item->get_Id());
             
          
             $pictures = $item->get_pictures();
-            $pictureCount = count($pictures);
+            $picture_count = count($pictures);
             
             
-            $winnerPseudo = $item->get_highest_bidder_pseudo();
+            $winner_pseudo = $item->get_highest_bidder_pseudo();
             
            
-            $sellerPseudo = $currentUser->get_Pseudo();
+            $seller_pseudo = $current_user->get_Pseudo();
             
             $sold_items[] = [
                 'id' => $item->get_Id(),
                 'title' => $item->get_Title(),
-                'pic_path' => $mainPicture ? $mainPicture->picture_path : null,
-                'picture_count' => $pictureCount,
-                'seller_pseudo' => $sellerPseudo,
+                'pic_path' => $main_picture ? $main_picture->picture_path : null,
+                'picture_count' => $picture_count,
+                'seller_pseudo' => $seller_pseudo,
                 'buy_now_price' => $item->get_Buy_Now_Price(),
                 'starting_bid' => $item->get_Starting_Bid(),
                 'max_bid' => $item->get_max_bid_time(),
@@ -54,7 +54,7 @@ class ControllerSales extends Controller {
                 'is_auction' => $item->get_Is_Auction(),
                 'has_buy_now' => $item->get_Has_buy_now_price(),
                 'is_direct_sale' => $item->get_Is_Direct_Sale(),
-                'winner_pseudo' => $winnerPseudo,
+                'winner_pseudo' => $winner_pseudo,
                 'closed_at' => $item->get_End_At() 
             ];
         }
@@ -65,7 +65,7 @@ class ControllerSales extends Controller {
             'back_url' => 'profile',
             'sold_items' => $sold_items,
             'statistics' => $statistics,
-            'currentUser' => $currentUser,
+            'current_user' => $current_user,
             'now' => $now
         ]);
     }
