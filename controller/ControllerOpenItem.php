c2212b01bdde806c152cf2f66e1de2231d2e86f2
zietraore <2610zitraore@student.epfc.eu>
2026-05-20 11:42:29 +0200

    fix(delete_confirm): adaptation du controller affin que qu'on puisse supprimer les unsold items

--- controller/ControllerOpenItem.php
+++ controller/ControllerOpenItem.php
@@ -262,7 +262,7 @@ private function load_item_or_fail(): ?Item {
             'is_auction' => $item->get_Is_Auction(),
             'starting_bid' => $item->get_Starting_Bid(),
             'auction_ended' => !$is_open && $item->get_Is_Auction(),
-            'can_delete' => $is_open && !$has_bids_time,
+            'can_delete' => (!$is_sold && !$has_bids_time) || ($item->has_buy_now_reached_time() && !$is_sold),
 
 
 
